3278c0ab638232371d220d19cc01f317ea4bcb57
HimikaP <prabhat.himika@gmail.com>
2023-03-08 23:02:11 +0530

    redefine

--- script.js
+++
@@ -1,6 +0,0 @@
-function addItem(){
-    let x = document.getElementById("inputIt").value;
-    const todo = document.createElement("p");
-    todo.innerHTML = x;
-    document.getElementById("todo-container").appendChild(todo);
-}
